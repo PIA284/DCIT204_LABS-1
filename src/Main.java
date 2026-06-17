fe209a8dc6c8c9ab77b856ba7a853e8f7b62f8df
PIA284 <piagbo-odo@st.ug.edu.gh>
2026-06-17 09:06:53 +0000

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,3 +3,46 @@ public class Main {
 
     }
 }
+Collect array size from the user
+        System.out.print("Enter array size: ");
+        int size = scanner.nextInt();
+        System.out.println(); 
+        int[] array = new int[size];
+
+       
+        for (int i = 0; i < size; i++) {
+            System.out.print("Enter element " + (i + 1) + ": ");
+            array[i] = scanner.nextInt();
+        }
+        System.out.println(); 
+
+        
+        System.out.print("Enter target value: ");
+        int target = scanner.nextInt();
+        System.out.println();
+
+        
+        int linearResult = Algorithm.linearSearch(array, target);
+        System.out.println("Linear Search Result:");
+        if (linearResult != -1) {
+            System.out.println("Target found at index " + linearResult);
+        } else {
+            System.out.println("Target not found");
+        }
+        System.out.println();
+
+        
+        Arrays.sort(array);
+
+    
+        int binaryResult = Algorithm.binarySearch(array, target);
+        System.out.println("Binary Search Result:");
+        if (binaryResult != -1) {
+            System.out.println("Target found at index " + binaryResult);
+        } else {
+            System.out.println("Target not found");
+        }
+
+        scanner.close();
+    }
+}
